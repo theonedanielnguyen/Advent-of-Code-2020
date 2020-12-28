68d6e914ab8a01418e0c50a0530d54328af05df3
Daniel Nguyen <theonedanielnguyen@gmail.com>
2020-12-27 23:07:59 -0800

    Day 8 Part 1 completed

--- Day-8/Day8.py
+++ Day-8/Day8.py
@@ -4,4 +4,51 @@ with open("C:/Users/Daniel/Desktop/AdventOfCode/Day-8/input.txt") as x:
         line = line[:-1]
         dataSet.append([line[:3], line[4:]])
 
-print(dataSet)
+indexTracker = []
+runner = 0
+acc = 0
+
+# def game(runner, acc, indexTracker):
+#     if runner in indexTracker:
+#         print(acc)
+#         return
+#     else:
+#         indexTracker.append(runner)
+#         if (dataSet[runner][0] == "nop"):
+#             runner += 1
+#         else:
+#             math = dataSet[runner][1][0]
+#             count = str(dataSet[runner][1][1:-1])
+#             if (dataSet[runner][0] == "acc"):
+#                 acc = eval(str(acc)+math+count)
+#                 runner += 1
+#             elif (dataSet[runner][0] == "jmp"):
+#                 runner = eval(str(runner)+math+count)
+#     return game(runner, acc, indexTracker)
+
+def game(runner, acc, indexTracker):
+    if runner in indexTracker:
+        print(acc)
+        return
+    else:
+        indexTracker.append(runner)
+        if (dataSet[runner][0] == "nop"):
+            runner += 1
+        else:
+            math = dataSet[runner][1][0]
+            count = int(dataSet[runner][1][1:])
+            if (dataSet[runner][0] == "acc"):
+                if (math == "+"):
+                    acc += count
+                    runner += 1
+                if (math == "-"):
+                    acc -= count
+                    runner += 1
+            elif (dataSet[runner][0] == "jmp"):
+                if (math == "+"):
+                    runner += count
+                if (math == "-"):
+                    runner -= count
+    return game(runner, acc, indexTracker)
+
+game(runner, acc, indexTracker)
