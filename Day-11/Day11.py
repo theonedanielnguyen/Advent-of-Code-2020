fc6d499a305ae3f9109a534bd3eeacd431477bd4
Daniel Nguyen <theonedanielnguyen@gmail.com>
2021-01-23 21:05:51 -0800

    Dinner push

---
+++ Day-11/Day11.py
@@ -0,0 +1,7 @@
+dataSet = []
+
+with open("C:/Users/Daniel/Desktop/AdventOfCode/Day-11/input.txt") as x:
+    for line in x:
+        line = line[:-1]
+        dataSet.append(int(line))
+##-----------------------------------------------##
