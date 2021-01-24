c5f2aa1eb04505b5e9cbaf334015b0f57b4b582e
Daniel Nguyen <theonedanielnguyen@gmail.com>
2021-01-23 22:37:08 -0800

    EOD push

--- Day-11/Day11.py
+++ Day-11/Day11.py
@@ -3,5 +3,16 @@ dataSet = []
 with open("C:/Users/Daniel/Desktop/AdventOfCode/Day-11/input.txt") as x:
     for line in x:
         line = line[:-1]
-        dataSet.append(int(line))
+        dataSet.append(line)
 ##-----------------------------------------------##
+
+print(dataSet[0][-1])
+
+# def planeSeating(data):
+#     newData = []
+
+#     for i in range(len(data)):
+
+
+
+#     return planeSeating(data)
